5b84ffcfb57950c41c41de7b171a635f0edb1a26
moustafa2121 <moustafa2121@gmail.com>
2024-02-14 06:33:43 +0400

    comment out admin

--- ProjectFlashback_b_p2/urls.py
+++ ProjectFlashback_b_p2/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     path('', include('ProjectFlashback_b_p2_app.urls')),
 ]
